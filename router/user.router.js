4d402eac0cbe6a99047e92dc6b32dea8ed606a72
CharlesRobart <robartcharles@gmail.com>
2024-01-25 15:45:51 +0100

    optimization of road's Get...

--- router/user.router.js
+++ router/user.router.js
@@ -5,9 +5,6 @@ const userRouter = Router();
 
 // list of endpoint 
 
-// Get all Users for testing purposes (GET /users)
-userRouter.get("/users", userController.getAllUsers);
-
 // Register a new User Endpoint
 userRouter.post("/register", userController.registerUser);
 
